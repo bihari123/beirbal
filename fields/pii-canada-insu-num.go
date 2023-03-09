8bc9b140e4b4da8459e24c64fbe1c44f6650d2a8
bihari123 <thakur.cs.tarun@gmail.com>
2023-03-09 19:58:32 +0530

    added indian aadhar and canada insurance

---
+++ fields/pii-canada-insu-num.go
@@ -0,0 +1,145 @@
+package fields
+
+import (
+	"example.com/utils"
+	"github.com/jdkato/prose/v2"
+)
+
+// CSIN = canada social insurance number
+var CSINLabel = "pii-indian-aadhar"
+
+type CSINEntry = utils.ProdigyOutput
+
+var sampleTextCSIN = []string{
+	"sin %v",
+	"social insurance %v",
+	"numero d'assurance sociale %v",
+	"sins %v",
+	"ssn %v",
+	"ssns %v",
+	"social security %v",
+	"numero d'assurance social %v",
+	"national identification number %v",
+	"national id %v",
+	"sin# %v",
+	"soc ins %v",
+	"social ins %v",
+	"NAS %v",
+	"numéro d’assurance social %v",
+	"numéro d’assurance sociale %v",
+	"assurance social %v",
+	"carte d’assurance sociale %v",
+}
+
+// training the money data for 3-7 figures bank amount
+var training_statements_CSIN = []utils.ProdigyOutput{
+	CSINEntry{
+		Text: sampleTextCSIN[0],
+		Spans: []prose.LabeledEntity{
+			{Start: 4, End: utils.GetWholeEntity(sampleTextCSIN[0], 4), Label: CSINLabel},
+		},
+	},
+	CSINEntry{
+		Text: sampleTextCSIN[1],
+		Spans: []prose.LabeledEntity{
+			{Start: 17, End: utils.GetWholeEntity(sampleTextCSIN[1], 17), Label: CSINLabel},
+		},
+	},
+	CSINEntry{
+		Text: sampleTextCSIN[2],
+		Spans: []prose.LabeledEntity{
+			{Start: 27, End: utils.GetWholeEntity(sampleTextCSIN[2], 27), Label: CSINLabel},
+		},
+	},
+	CSINEntry{
+		Text: sampleTextCSIN[3],
+		Spans: []prose.LabeledEntity{
+			{Start: 5, End: utils.GetWholeEntity(sampleTextCSIN[3], 5), Label: CSINLabel},
+		},
+	},
+	CSINEntry{
+		Text: sampleTextCSIN[4],
+		Spans: []prose.LabeledEntity{
+			{Start: 4, End: utils.GetWholeEntity(sampleTextCSIN[4], 4), Label: CSINLabel},
+		},
+	},
+
+	CSINEntry{
+		Text: sampleTextCSIN[5],
+		Spans: []prose.LabeledEntity{
+			{Start: 5, End: utils.GetWholeEntity(sampleTextCSIN[5], 5), Label: CSINLabel},
+		},
+	},
+	CSINEntry{
+		Text: sampleTextCSIN[6],
+		Spans: []prose.LabeledEntity{
+			{Start: 16, End: utils.GetWholeEntity(sampleTextCSIN[6], 16), Label: CSINLabel},
+		},
+	},
+	CSINEntry{
+		Text: sampleTextCSIN[7],
+		Spans: []prose.LabeledEntity{
+			{Start: 26, End: utils.GetWholeEntity(sampleTextCSIN[7], 26), Label: CSINLabel},
+		},
+	},
+	CSINEntry{
+		Text: sampleTextCSIN[8],
+		Spans: []prose.LabeledEntity{
+			{Start: 31, End: utils.GetWholeEntity(sampleTextCSIN[8], 31), Label: CSINLabel},
+		},
+	},
+	CSINEntry{
+		Text: sampleTextCSIN[9],
+		Spans: []prose.LabeledEntity{
+			{Start: 12, End: utils.GetWholeEntity(sampleTextCSIN[9], 12), Label: CSINLabel},
+		},
+	},
+	CSINEntry{
+		Text: sampleTextCSIN[10],
+		Spans: []prose.LabeledEntity{
+			{Start: 5, End: utils.GetWholeEntity(sampleTextCSIN[10], 5), Label: CSINLabel},
+		},
+	},
+	CSINEntry{
+		Text: sampleTextCSIN[11],
+		Spans: []prose.LabeledEntity{
+			{Start: 8, End: utils.GetWholeEntity(sampleTextCSIN[11], 8), Label: CSINLabel},
+		},
+	},
+	CSINEntry{
+		Text: sampleTextCSIN[12],
+		Spans: []prose.LabeledEntity{
+			{Start: 11, End: utils.GetWholeEntity(sampleTextCSIN[12], 11), Label: CSINLabel},
+		},
+	},
+	CSINEntry{
+		Text: sampleTextCSIN[13],
+		Spans: []prose.LabeledEntity{
+			{Start: 4, End: utils.GetWholeEntity(sampleTextCSIN[13], 4), Label: CSINLabel},
+		},
+	},
+	CSINEntry{
+		Text: sampleTextCSIN[14],
+		Spans: []prose.LabeledEntity{
+			{Start: 26, End: utils.GetWholeEntity(sampleTextCSIN[14], 26), Label: CSINLabel},
+		},
+	},
+	CSINEntry{
+		Text: sampleTextCSIN[15],
+		Spans: []prose.LabeledEntity{
+			{Start: 27, End: utils.GetWholeEntity(sampleTextCSIN[15], 27), Label: CSINLabel},
+		},
+	},
+	CSINEntry{
+		Text: sampleTextCSIN[16],
+		Spans: []prose.LabeledEntity{
+			{Start: 17, End: utils.GetWholeEntity(sampleTextCSIN[16], 17), Label: CSINLabel},
+		},
+	},
+	CSINEntry{
+		Text: sampleTextCSIN[17],
+		Spans: []prose.LabeledEntity{
+			{Start: 26, End: utils.GetWholeEntity(sampleTextCSIN[17], 26), Label: CSINLabel},
+		},
+	},
+}
---
+++ fields/pii-indian-aadhar.go
@@ -0,0 +1,87 @@
+package fields
+
+import (
+	"example.com/utils"
+	"github.com/jdkato/prose/v2"
+)
+
+var AadharLabel = "pii-indian-aadhar"
+
+type AadharEntry = utils.ProdigyOutput
+
+var sampleTextAadhar = []string{
+	"The indian unique identification for this account is %v",
+	"His indian unique identification for is %v",
+	"Aadhar Card number %v",
+	"Aadhar ID %v",
+	"UDIAI ID %v",
+	"UDAIA number %v",
+	"aadhar# %v",
+	"uid %v",
+	"aadhaar %v",
+	"आधार" + " %v",
+}
+
+// training the money data for 3-7 figures bank amount
+var training_statements_aadhar = []utils.ProdigyOutput{
+	AadharEntry{
+		Text: sampleTextAadhar[0],
+		Spans: []prose.LabeledEntity{
+			{Start: 53, End: utils.GetWholeEntity(sampleTextAadhar[0], 53), Label: AadharLabel},
+		},
+	},
+	AadharEntry{
+		Text: sampleTextAadhar[1],
+		Spans: []prose.LabeledEntity{
+			{Start: 40, End: utils.GetWholeEntity(sampleTextAadhar[1], 40), Label: AadharLabel},
+		},
+	},
+	AadharEntry{
+		Text: sampleTextAadhar[2],
+		Spans: []prose.LabeledEntity{
+			{Start: 19, End: utils.GetWholeEntity(sampleTextAadhar[2], 19), Label: AadharLabel},
+		},
+	},
+	AadharEntry{
+		Text: sampleTextAadhar[3],
+		Spans: []prose.LabeledEntity{
+			{Start: 10, End: utils.GetWholeEntity(sampleTextAadhar[3], 10), Label: AadharLabel},
+		},
+	},
+	AadharEntry{
+		Text: sampleTextAadhar[4],
+		Spans: []prose.LabeledEntity{
+			{Start: 13, End: utils.GetWholeEntity(sampleTextAadhar[4], 13), Label: AadharLabel},
+		},
+	},
+	AadharEntry{
+		Text: sampleTextAadhar[5],
+		Spans: []prose.LabeledEntity{
+			{Start: 8, End: utils.GetWholeEntity(sampleTextAadhar[5], 8), Label: AadharLabel},
+		},
+	},
+	AadharEntry{
+		Text: sampleTextAadhar[6],
+		Spans: []prose.LabeledEntity{
+			{Start: 4, End: utils.GetWholeEntity(sampleTextAadhar[6], 4), Label: AadharLabel},
+		},
+	},
+
+	AadharEntry{
+		Text: sampleTextAadhar[7],
+		Spans: []prose.LabeledEntity{
+			{Start: 8, End: utils.GetWholeEntity(sampleTextAadhar[7], 8), Label: AadharLabel},
+		},
+	},
+
+	AadharEntry{
+		Text: sampleTextAadhar[8],
+		Spans: []prose.LabeledEntity{
+			{Start: 5, End: utils.GetWholeEntity(sampleTextAadhar[8], 5), Label: AadharLabel},
+		},
+	},
+}
+
+func GetTextForFieldAadhar(aadharId string) string {
+	return utils.GetRandomEntry(training_statements_aadhar, aadharId)
+}
