ccf4ee409e0d41ae2ca992f53bd51967f357a376
Bihari123 <thakur.cs.tarun@gmail.com>
2023-03-13 22:47:52 +0530

    ready for demo

--- main.go
+++ main.go
@@ -4,21 +4,22 @@ import (
 	"fmt"
 	"os/exec"
 
+	"example.com/testNtrain/test"
 	"example.com/testNtrain/train"
 )
 
 func main() {
 	fmt.Println("loading raw data into prodigy json format\n ")
 	train.LoadRawData()
-	/*
-		fmt.Println("\n\ntraining golang prose lib")
-		train.Train("./output/json/testFile.jsonl", "demo_model")
-		fmt.Println("\n\ntesting using in golang prose lib")
-		test.Test("./output/json/testFile.jsonl", "./output/model/demo_model")
-	*/
+
+	fmt.Println("\n\ntraining golang prose lib")
+	train.Train("./output/json/testFile.jsonl", "demo_model")
+
+	fmt.Println("\n\ntesting using in golang prose lib")
+	test.Test("./output/json/testFile.jsonl", "./output/model/demo_model")
+
 	fmt.Println("training using spaCy")
 
-	// os.Chdir("./spaCy-integration")
 	cmd := exec.Command("python3", "./spaCy-integration/ve_nlp_pipeline.py")
 
 	out, err := cmd.Output()
